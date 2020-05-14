3b5812893dd132af929de8336693225c33428af8
Duarte Carrilho da Graça <duarte@duarte.gd>
2020-05-14 16:41:21 +0200

    allow flexible standfirst height

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,8 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
+
+
+// let standfirst = document.querySelector('.content__standfirst');
+// let textColumn = document.querySelector('.content--interactive .content__main-column--interactive')
+// textColumn.insertBefore(standfirst, textColumn.firstChild);
+
